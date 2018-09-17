95b3f99764730ad1ed4ef36016d32891d393e181
Hsuan-Lun <f97548021@ntu.edu.tw>
2018-09-17 21:59:26 +0800

    add new feature

--- app/src/main/assets/www/app.js
+++ app/src/main/assets/www/app.js
@@ -1,32 +1,72 @@
-function setName(){
-    person = document.getElementById("inputname").value ;
-    if (person===""){
-    person = "Guest" ;
+function helloVicky (){
+	alert("Hi Vicky!");
+}
+
+function openTab(evt, cityName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
-    localStorage.setItem("lastname",person);
-    sayHello(person) ;
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
-function demoClick(){
-    person = "demo" ;
-    localStorage.setItem("lastname",person) ;
-    sayHello(person);
+function addInfo(){
+    let field, account, password, password_confirm
+    field = document.getElementById("field").value ;
+    account = document.getElementById("account").value ;
+    password = document.getElementById("password").value ;
+    password_confirm = document.getElementById("password_confirm").value ;
+    if (password != password_confirm){
+        alert("密碼確認過程有誤 !");
+    }else{
+
+        if (localStorage.getItem("FIELD")==undefined){
+            var FIELD = [field];
+            var ACCOUNT = [account];
+            var PASSWORD = [password];
+        }else{
+            var FIELD = [field];
+            var ACCOUNT = [account];
+            var PASSWORD = [password];
+            FIELD.unshift(localStorage.getItem("FIELD"));
+            ACCOUNT.unshift(localStorage.getItem("ACCOUNT"));
+            PASSWORD.unshift(localStorage.getItem("PASSWORD"));
+        }
+
+        localStorage.setItem('FIELD',FIELD);
+        localStorage.setItem('ACCOUNT',ACCOUNT);
+        localStorage.setItem('PASSWORD',PASSWORD);
+
+        alert("Save Complete!");
+        clearInputInfo();
+    }
+
 }
 
-function sayHello(person){
-console.log(person);
-person = localStorage.getItem("lastname") ;
-if (person===null | typeof person == "undefined"){
-person = "Guest" ;
-localStorage.setItem("lastname",person);
-}else{
-person = localStorage.getItem("lastname") ;
-}
-document.getElementById("demo").innerHTML = "Hello " + person + "! How are you today?";
+function clearInputInfo(){
+	document.getElementById("field").value ="";
+    document.getElementById("account").value ="";
+    document.getElementById("password").value ="";
+    document.getElementById("password_confirm").value ="";
 }
 
-function sayGoodbye(){
-console.log("Bye bye ~ " + localStorage.getItem("lastname"));
-//localStorage.removeItem("lastname");
-alert("BYE");
+var open = 'glyphicon-eye-open';
+var close = 'glyphicon-eye-close';
+var ele = document.getElementById('password');
+document.getElementById('toggleBtn').onclick = function() {
+    if( this.classList.contains(open) ) {
+        ele.type="text";
+        this.classList.remove(open);
+        this.className += ' '+close;
+    } else {
+        ele.type="password";
+        this.classList.remove(close);
+        this.className += ' '+open;
+    }
 }
