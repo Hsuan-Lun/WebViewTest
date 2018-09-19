e1d618330970b79c2cb907e45659872033e297a8
Hsuan-Lun <f97548021@ntu.edu.tw>
2018-09-19 23:37:43 +0800

    add new feature

--- app/src/main/assets/www/app.js
+++ app/src/main/assets/www/app.js
@@ -28,58 +28,104 @@ function openTab(evt, cityName) {
 }
 
 function addInfo(){
-    let field, account, password, password_confirm;
-    field = document.getElementById("field").value ;
-    account = document.getElementById("account").value ;
-    password = document.getElementById("password").value ;
-    password_confirm = document.getElementById("password_confirm").value ;
-    if (password !== password_confirm){
-        //alert("密碼確認過程有誤 !");
-        toastr.options = {"positionClass":"toast-bottom-center"};
-        toastr.error("密碼確認過程有誤 !").css("font-size","16px");
-    }else{
+    if(checkInputInfo(false)) {
+        let field, account, password, password_confirm;
+        field = document.getElementById("field").value;
+        account = document.getElementById("account").value;
+        password = document.getElementById("inputPW").value;
+        password_confirm = document.getElementById("inputPW_confirm").value;
+        if (password !== password_confirm) {
+            //alert("密碼確認過程有誤 !");
+            toastr.options = {"positionClass": "toast-bottom-center"};
+            toastr.error("密碼確認過程有誤 !").css("font-size", "16px");
+        } else {
 
-        if (localStorage.getItem("FIELD")==undefined){
-            FIELD = [field];
-            ACCOUNT = [account];
-            PASSWORD = [password];
-        }else{
-            FIELD = [field];
-            ACCOUNT = [account];
-            PASSWORD = [password];
-            FIELD.unshift(localStorage.getItem("FIELD"));
-            ACCOUNT.unshift(localStorage.getItem("ACCOUNT"));
-            PASSWORD.unshift(localStorage.getItem("PASSWORD"));
+            if (localStorage.getItem("FIELD") == undefined) {
+                FIELD = [field];
+                ACCOUNT = [account];
+                PASSWORD = [password];
+            } else {
+                if (!checkDuplicatedField()) {
+                    FIELD = [field];
+                    ACCOUNT = [account];
+                    PASSWORD = [password];
+                    FIELD.unshift(localStorage.getItem("FIELD"));
+                    ACCOUNT.unshift(localStorage.getItem("ACCOUNT"));
+                    PASSWORD.unshift(localStorage.getItem("PASSWORD"));
+
+                    localStorage.setItem('FIELD', FIELD);
+                    localStorage.setItem('ACCOUNT', ACCOUNT);
+                    localStorage.setItem('PASSWORD', PASSWORD);
+
+                    //alert("Save Complete!");
+                    toastr.options = {"positionClass": "toast-bottom-center"};
+                    toastr.success("Save Complete!").css("font-size", "16px");
+                    clearInputInfo(false);
+                }
+            }
         }
-
-        localStorage.setItem('FIELD',FIELD);
-        localStorage.setItem('ACCOUNT',ACCOUNT);
-        localStorage.setItem('PASSWORD',PASSWORD);
-
-        //alert("Save Complete!");
-        toastr.options = {"positionClass":"toast-bottom-center"};
-        toastr.success("Save Complete!").css("font-size","16px");
-        clearInputInfo(false);
+    }else{
+        toastr.options = {"positionClass": "toast-bottom-center"};
+        toastr.error("尚有資料未填寫 !").css("font-size", "16px");
     }
-
 }
+
+function checkDuplicatedField(){
+    FIELD = localStorage.getItem("FIELD");
+    let field = document.getElementById("field").value ;
+    let ind = FIELD.toLowerCase().split(",").indexOf(field);
+    if (ind<0){
+        return false;
+    }else{
+        toastr.error("之前輸入過囉!!");
+        return true;
+    }
+}
+
 
 function clearInputInfo(hasNotice){
-    if(hasNotice===undefined){let hasNotice = false;}
+    if(checkInputInfo(true)) {
+        if (hasNotice === undefined) {
+            var hasNotice = false;
+        }
 
-    if (hasNotice){
-        var r = confirm("確定要清除填寫內容 ?!");
-    }else{
-    	var r = true;
-    }
+        if (hasNotice) {
+            var r = confirm("確定要清除填寫內容 ?!");
+        } else {
+            var r = true;
+        }
 
-    if(r===true) {
-        document.getElementById("field").value = "";
-        document.getElementById("account").value = "";
-        document.getElementById("password").value = "";
-        document.getElementById("password_confirm").value = "";
+        if (r === true) {
+            document.getElementById("field").value = "";
+            document.getElementById("account").value = "";
+            document.getElementById("inputPW").value = "";
+            document.getElementById("inputPW_confirm").value = "";
+        }
     }
 }
+
+function checkInputInfo(istoRemove){
+    if(istoRemove) {
+        if (document.getElementById("field").value === "" &&
+            document.getElementById("account").value === "" &&
+            document.getElementById("inputPW").value === "" &&
+            document.getElementById("inputPW_confirm").value === "") {
+            return false;
+        } else {
+            return true;
+        }
+    }else{
+        if (document.getElementById("field").value === "" ||
+            document.getElementById("account").value === "" ||
+            document.getElementById("inputPW").value === "" ||
+            document.getElementById("inputPW_confirm").value === "") {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
 
 function toggler(e,idName) {
     if( e.className === 'showPW' ) {
@@ -94,17 +140,23 @@ function toggler(e,idName) {
 }
 
 function parseFIELD(){
-    let input="" ;
-    let txt = localStorage.getItem("FIELD"); //typeof a is string
-	let array = txt.split(","); //typeof array is object
-    let a = document.getElementById("showField");
-    for(let i=0;i<array.length;i++){
-        input = input + "<li class='showitem' id='item" + i + "' onclick='returnData(this)'>" + array[i] + "</li>" ;
+    if(document.getElementById("showFieldBtn").innerText==="列出所有") {
+        let input = "";
+        let txt = localStorage.getItem("FIELD"); //typeof a is string
+        let array = txt.split(","); //typeof array is object
+        let a = document.getElementById("showField");
+        for (let i = 0; i < array.length; i++) {
+            input = input + "<li class='showitem' id='item" + i + "' onclick='returnData(this)'>" + array[i] + "</li>";
+        }
+        a.innerHTML = input;
+        document.getElementById("showFieldBtn").innerText="收起清單";
+    }else{
+        document.getElementById("showField").innerHTML = "";
+        document.getElementById("showFieldBtn").innerText="列出所有";
     }
-    a.innerHTML = input ;
 }
 
-function returnData(e){
+function returnData(itemhandle){
 	toastr.options = {
     "closeButton": false,
     "debug": false,
@@ -121,16 +173,16 @@ function returnData(e){
     "hideEasing": "linear",
     "showMethod": "fadeIn",
     "hideMethod": "fadeOut"
-	}
+	};
     //let str = "Don't push " + e.id ;
     //toastr.info(str);
-    field = localStorage.getItem("FIELD");
-    account = localStorage.getItem("ACCOUNT");
-    password = localStorage.getItem("PASSWORD");
+    let field = localStorage.getItem("FIELD");
+    let account = localStorage.getItem("ACCOUNT");
+    let password = localStorage.getItem("PASSWORD");
     FIELD = field.split(",");
     ACCOUNT = account.split(",");
     PASSWORD = password.split(",");
-    let ind = parseInt(e.id[e.id.length-1]) ;
+    let ind = parseInt(itemhandle.id[itemhandle.id.length-1]) ;
     let str = "<span font-size:'16px'>名稱: </span><span margin-top='0px'>"+ FIELD[ind]+ "</span><br>" +
              "<span font-size:'16px'>帳號: </span><span margin-top='0px'>"+ ACCOUNT[ind]+ "</span><br>" +
              "<span font-size:'16px'>密碼: </span><span margin-top='0px'>"+PASSWORD[ind]+"</span>";
@@ -138,6 +190,6 @@ function returnData(e){
 }
 
 
-function underconstruction(){
-	toastr.info("還沒好唷~再等等").css("font-size","20px");
+function clearStorage(){
+	toastr.warning("確定放棄治療?!")
 }
